1679cf7cf1f8390b1253be3ab47a64c7d38f2088
NANDO <amnando.bs@gmail.com>
2025-12-31 18:10:38 +0330

    change the model name from Profile to Account and create serializer file.

---
+++ src/apps/accounts/serializers.py
@@ -0,0 +1,6 @@
+from rest_framework import serializers
+from apps.accounts.models import Account
+class AccountSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Account
+        fields = '__all__'
