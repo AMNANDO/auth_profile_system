10b55b5f0cfba4a4b09bb176e9c9923da98d8b43
NANDO <amnando.bs@gmail.com>
2025-12-31 18:07:14 +0330

    add Profile model to accounts app and relate it to User model.

--- src/apps/accounts/models.py
+++ src/apps/accounts/models.py
@@ -1,3 +1,15 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Profile(models.Model):
+    user=models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    name=models.CharField(max_length=100)
+    age=models.PositiveIntegerField()
+    bio=models.TextField(blank=True)
+    email=models.EmailField(unique=True)
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
