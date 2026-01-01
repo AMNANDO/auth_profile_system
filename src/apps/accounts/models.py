f7c2f05a813697dd42999aa68b5ba2bc5c48d127
NANDO <amnando.bs@gmail.com>
2026-01-01 18:30:02 +0330

    rename the accounts/models.AccountModle.userField.related_name from profile to account.

--- src/apps/accounts/models.py
+++ src/apps/accounts/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Account(models.Model):
-    user=models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    user=models.OneToOneField(User, on_delete=models.CASCADE, related_name='account')
     name=models.CharField(max_length=100)
     age=models.PositiveIntegerField()
     bio=models.TextField(blank=True)
