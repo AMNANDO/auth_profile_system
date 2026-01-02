d24bf072522066dd753a1c98fa717d18de6507e1
NANDO <amnando.bs@gmail.com>
2026-01-02 11:16:57 +0330

    add base file to test directory for extend all test files from this file.

---
+++ src/tests/base.py
@@ -0,0 +1,28 @@
+from rest_framework.test import APITestCase
+from django.contrib.auth.models import User
+from apps.accounts.models import Account
+
+class BaseAccountTest(APITestCase):
+    @classmethod
+    def setUpTestData(cls):
+        # Create a test user
+        cls.user = User.objects.create_user(username='whoami', password='adminnistam')
+        # Create admin user
+        cls.admin = User.objects.create_superuser(username='admin', password='adminadmin')
+        # Create a test account linked to the user
+        cls.active_account = Account.objects.create(
+            user=cls.user,
+            name='Test User',
+            age=30,
+            bio='This is a test user.',
+            is_active=True,
+        )
+        cls.inactive_account = Account.objects.create(
+            user=cls.user,
+            name='Inactive User',
+            age=25,
+            bio='This is an inactive user.',
+            is_active=False,
+        )
+        def authenticate(self, user):
+            self.client.force_authenticate(user=user)
